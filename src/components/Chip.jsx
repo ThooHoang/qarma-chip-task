74f2744defddb01625195402e6d47092a666f155
Your Name <eaa24quho@students.eaaa.dk>
2025-12-20 09:51:22 +0100

    the X button color fix

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -21,14 +21,14 @@ const Chip = ({ label, onRemove }) => {
                 <button 
                     type="button" 
                     onClick={onRemove} 
-                    className="flex items-center justify-center w-full h-full hover:bg-chip-remove-button-hover rounded-round focus:border-color-focus border border-transparent border-width-focus"
+                    className="flex items-center justify-center w-full h-full hover:bg-chip-remove-button-hover rounded-round focus:border-color-focus border border-transparent border-width-focus text-chip-button-level-2 hover:text-chip-remove-button-hover"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="12" 
                         height="12" 
                         viewBox="0 0 256 256"
-                        fill="#6E6E6E"
+                        fill="currentColor"
                     >
                         <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
                     </svg>
